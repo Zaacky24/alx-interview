28803a11937b4aa8a6cc9dcab83d7dc34c6d1014
Zaacky24 <zaacky98@gmail.com>
2023-06-28 21:32:05 +0000

    new changes

--- 0-promise.js
+++ 0-promise.js
@@ -1,15 +1,16 @@
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Perform API request or any asynchronous operation here
+    // If the operation is successful, call resolve() with the desired response
+    // If there's an error, call reject() with the error message
+
+    // For example, let's simulate a successful API response after a delay
+    setTimeout(() => {
+      const response = "API response";
+      resolve(response);
+    }, 2000);
+  });
 }
 
-export function getLast() {
-  return ' is okay';
-}
+export default getResponseFromAPI;
 
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
